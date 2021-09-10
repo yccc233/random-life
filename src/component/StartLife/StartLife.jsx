7bbab20e223b78f40f7620732299c40587fec291
yccc233 <1099312164@qq.com>
2021-09-10 16:10:58 +0800

    差一个核心逻辑和布局

--- src/component/StartLife/StartLife.jsx
+++ src/component/StartLife/StartLife.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import {Affix, Button, Popover, Drawer, Tooltip} from "antd";
+import "./StartLife.css";
 const $ = require('jquery');
 
 const dbTable = 'roles';
@@ -31,10 +33,77 @@ export class StartLife extends React.Component {
     }
 
     render() {
+        let baseProp = {
+            name: this.state.name,
+            sex: this.state.sex,
+            familyPoint: this.state.familyPoint,
+            iqPoint: this.state.iqPoint,
+            bodyPoint: this.state.bodyPoint,
+            luckPoint: this.state.luckPoint
+        }
         return (
-            <div>
-                <p>hello this is your life!</p>
-            </div>
+            <>
+                <DrawerProp
+                    text="属性啥来着？"
+                    toolTip="点击弹出属性框，点击黑色区域关闭弹窗！"
+                    params={baseProp}
+                />
+                <div>
+                {/*  业务逻辑代码在这里写  */}
+                </div>
+            </>
         );
     }
 }
+
+// 属性展示，抽屉展示
+const DrawerProp = (props) => {
+    const [visible, setVisible] = useState(false);
+
+    const showDrawer = () => {
+        setVisible(true);
+    };
+
+    const onClose = () => {
+        setVisible(false);
+    };
+
+    let sex = props.params.sex === "male" ? "男" : "女";
+
+    return (
+        <>
+            <Tooltip
+                placement="rightBottom"
+                title={props.toolTip}
+                mouseEnterDelay="1"
+                autoAdjustOverflow={true}
+            >
+                <Affix offsetTop={10} offsetBottom={10}>
+                    <Button
+                        onClick={showDrawer}
+                        className="propAffix"
+                    >{props.text}</Button>
+                </Affix>
+            </Tooltip>
+
+            <Drawer title={`Hi, 你的娃儿是${props.params.name}呀，还是个${sex}娃儿！`} placement="right" onClose={onClose} visible={visible}>
+                <p>
+                    <span className="propLabel">家庭属性：</span>
+                    <span className="propValue"><u>{props.params.familyPoint}</u></span>
+                </p>
+                <p>
+                    <span className="propLabel">智商属性：</span>
+                    <span className="propValue"><u>{props.params.iqPoint}</u></span>
+                </p>
+                <p>
+                    <span className="propLabel">身体属性：</span>
+                    <span className="propValue"><u>{props.params.bodyPoint}</u></span>
+                </p>
+                <p>
+                    <span className="propLabel">辛运属性：</span>
+                    <span className="propValue"><u>{props.params.luckPoint}</u></span>
+                </p>
+            </Drawer>
+        </>
+    );
+};
